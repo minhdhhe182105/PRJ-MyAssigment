6c505f7ae24cad03cd21861f5f45765dbcc8d672
minhdhhe182105 <minhdhhe182105@fpt.edu.vn>
2025-02-26 16:54:16 +0700

    login
    login

--- MyAssigment1/src/java/Controller/auth/Login.java
+++ MyAssigment1/src/java/Controller/auth/Login.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controller;
+package Controller.auth;
 
 import Context.UserDBContext;
 import jakarta.servlet.ServletException;
@@ -32,7 +32,7 @@ public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("view/auth/login.html").forward(request, response);
+        request.getRequestDispatcher("../view/auth/login.html").forward(request, response);
     }
 
     /**
